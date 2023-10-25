99364bf050c37725a2d8ac33b9126365266458d6
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-10-25 13:08:01 -0500

    feat: :recycle: Replace AddRolAsync by AddRoleAsync
    @Gonther01

--- WebApi/Services/IUserService.cs
+++ WebApi/Services/IUserService.cs
@@ -10,7 +10,7 @@ public interface IUserService
 {
     Task<string> RegisterAsync(RegisterDto model);
     Task<DataUserDto> GetTokenAsync(LoginDto model);
-    Task<string> AddRolAsync(AddRoleDto model);
+    Task<string> AddRoleAsync(AddRoleDto model);
     Task<DataUserDto> RefreshTokenAsync(string refreshToken);
     
 }
