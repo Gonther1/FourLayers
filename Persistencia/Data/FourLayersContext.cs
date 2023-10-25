99618118e1588526a8df5b4c63e8fc8fd343457f
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-10-25 13:06:46 -0500

    feat: :sparkles: Add DbSet to file context
    @Gonther1

--- Persistencia/Data/FourLayersContext.cs
+++ Persistencia/Data/FourLayersContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Dominio.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistencia.Data;
@@ -12,6 +13,10 @@ public class FourLayersContext : DbContext
     public FourLayersContext(DbContextOptions options) : base(options)
     {
     }
+
+    public DbSet<Rol> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRol> UsersRoles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
